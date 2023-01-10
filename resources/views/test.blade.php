1baa6b4c78bf756a9c91c3718a41bcb5a876bdd0
hilmifawwaz <muhammadhilmifawwaz@gmail.com>
2023-01-10 22:56:17 +0700

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+  <style>
+    img{
+      float: none;
+    }
+  </style>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    {{-- @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"); --}}
+    <title>Hello, world!</title>
+  </head>
+
+  <header>
+    <nav class="navbar navbar-light" style="background-color: #3C8DBC">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">
+          <img src="img/logo.png" alt="gambar_keren" width="100%" height="120" class="d-inline-block align-text-top">
+        </a>
+      </div>
+    </nav>
+    <nav class="navbar navbar-light" style="background-color: #186D9D">
+      <div class="container-fluid">
+        <button class="btn" style="background-color: #186D9D" disabled></button>
+        <button class="btn btn-primary d-flex" style="background-color: #3C8DBC">
+          <i class="bi bi-person-fill"></i>
+          LOGIN
+        </button>
+      </div>
+    </nav>
+  </header>
+
+  <body>
+    
+    <div class="container">
+      <h1>TEST!</h1>
+    </div>
+
+  </body>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+</html>
